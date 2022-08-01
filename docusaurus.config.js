f5f66356b1b963ff3e12b2b8693ba7c2365d0847
Marcus Olsson <8396880+marcusolsson@users.noreply.github.com>
2022-08-01 09:27:57 +0200

    Enable DocSearch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -102,6 +102,29 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
           },
         ],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "3V8MQLOMH5",
+
+        // Public API key: it is safe to commit it
+        apiKey: "6cae981f4dfca4c6f308650dfd1b1dce",
+
+        indexName: "obsidian-plugin",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
       footer: {
         links: [
           {
