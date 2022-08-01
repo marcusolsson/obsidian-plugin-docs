0eabbe427b7b6715a3157fa75ff4078b3f6bef27
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-08-01 15:55:55 +0200

    patch

--- tools/alfred-workflow/obsi-marcus-doc-search.js
+++ tools/alfred-workflow/obsi-marcus-doc-search.js
@@ -4,6 +4,12 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const alfredMatcher = (str) => str.replace (/[-()_/.]/g, " ") + " " + str + " ";
 
+String.prototype.capitalizeWords = function () {
+	return this.replace(/\w+/g, word => word.charAt(0).toUpperCase() + word.slice(1));
+};
+
+//------------------------------------------------------------------------------
+
 const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/repos/marcusolsson/obsidian-plugin-docs/git/trees/main?recursive=1"'))
 	.tree
 	.filter(file => file.path.startsWith("docs/"))
@@ -12,11 +18,19 @@ const workArray = JSON.parse(app.doShellScript('curl -s "https://api.github.com/
 		const subsitePath = file.path.slice(5, -3);
 
 		const displayTitle = subsitePath
-			.replace(/.*\//, "")
+			.replace(/.*\//, "") // show only file name
+			.capitalizeWords()
+			.replaceAll("-", " ");
+
+		const category = subsitePath
+			.replace(/(.*)\/.*/, "$1") // only parent
+			.replaceAll ("/", " → ") // nicer tree
+			.capitalizeWords()
 			.replaceAll("-", " ");
 
 		return {
 			"title": displayTitle,
+			"subtitle": category,
 			"match": alfredMatcher (subsitePath),
 			"arg": `https://marcus.se.net/obsidian-plugin-docs/${subsitePath}`,
 			"uid": subsitePath,
